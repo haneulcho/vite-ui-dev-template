2fe75af6953e7857ec69637922909e50e4023806
Haneul Cho <stuykr@naver.com>
2025-09-05 16:41:58 +0900

    fix: 엔트리 스크립트 파일 분리

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,5 @@
 // SCSS 파일 import
-import '../css/style.scss';
+import '@/css/style.scss';
 
-// 기타 JavaScript 코드들
-console.log('Vite + SCSS 프로젝트가 시작되었습니다!');
-
-// DOM이 로드된 후 실행
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('DOM이 로드되었습니다.');
-});
+// JS 파일 import
+import '@/js/app.js';
