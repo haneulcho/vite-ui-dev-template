c5be5dc14b927add563be90eae3a9e1d13401f85
Haneul Cho <stuykr@naver.com>
2025-09-08 04:45:05 +0900

    feat: section6 이미지 클릭 기능 추가

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,22 @@ $(document).on('ready', function () {
     $currentLi.toggleClass('active');
     $currentAnswer.stop(true, true).slideToggle(300);
   });
+
+  // Section6 Sticky Images 클릭 기능
+  $(document).on('click', '.sticky-images .photo-link', function (e) {
+    e.preventDefault();
+
+    const $currentLi = $(this).closest('li');
+    const $allLis = $('.sticky-images ul > li');
+    const currentIndex = $allLis.index($currentLi);
+    const nextIndex = (currentIndex + 1) % $allLis.length;
+    const $nextLi = $allLis.eq(nextIndex);
+
+    // 모든 li에서 클래스 제거
+    $allLis.removeClass('active');
+    // 다음 이미지에 active 클래스 추가 (CSS delay로 자연스럽게 나타남)
+    $nextLi.addClass('active');
+  });
 });
 
 $window.on('resize', function () {});
